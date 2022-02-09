a6733c877791e7488b2622a13f5056be993e3543
whatever2501 <luiz_nando_vieira@hotmail.com>
2022-02-08 22:31:36 -0300

    classes

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -4,7 +4,7 @@ package br.com.dio.desafio.dominio;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello world");
 
 	}
 
