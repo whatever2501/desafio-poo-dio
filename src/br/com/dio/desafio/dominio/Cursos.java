16890c2c33afbcd09cc2a23595f57044e01e1d8d
whatever2501 <luiz_nando_vieira@hotmail.com>
2022-02-09 21:50:46 -0300

    Devs e Bootcamp

--- src/br/com/dio/desafio/dominio/Cursos.java
+++ src/br/com/dio/desafio/dominio/Cursos.java
@@ -1,5 +1,27 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Cursos extends Conteudo {
+	
+	
+	private int cargaHoraria;
+	
+	
+	public int getCargaHoraria() {
+		return cargaHoraria;
+	}
+	public void setCargaHoraria(int cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+	@Override
+	public String toString() {
+		return "Cursos [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
+	}
+	@Override
+	public double calcularXp() {
+		
+		return XP_PADRAO * cargaHoraria ; 
+	}
+	
+	
 
 }
