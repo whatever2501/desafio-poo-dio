1edaa7758c3f14c63b4182bc35c2575ebf33de74
whatever2501 <luiz_nando_vieira@hotmail.com>
2022-02-09 21:29:44 -0300

    Mentoria e cursos

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -1,10 +1,30 @@
 package br.com.dio.desafio.dominio;
 
+import java.time.LocalDate;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		
+		Cursos curso1 = new Cursos();
+		curso1.setTitulo("curso java");
+		curso1.setCargaHoraria(8);
+		curso1.setDescricao("Desicao");
+		
+		Cursos curso2 = new Cursos();
+		curso2.setTitulo("curso javascript");
+		curso2.setCargaHoraria(23);
+		curso2.setDescricao("Desissscao");
+		
+		System.out.println(curso1.toString());
+		System.out.println(curso2.toString());
+		
+		Mentorias mentoria1 = new Mentorias();
+		mentoria1.setTitulo("Mentoria java");
+		mentoria1.setDescricao("Mentoria java descricao");
+		mentoria1.setData(LocalDate.now());
+		
+		System.out.println(mentoria1.toString());
 
 	}
 
